8ef51acd26345b57adfa9996525da7fbbc61cff3
omidahomi <oiqbal@sjnma.org>
2017-04-27 08:47:58 -0500

    [finished project]

--- xtra.py
+++ xtra.py
@@ -1,9 +1,12 @@
 def mux_letters(inputstring, numberoftimes):
-	xtra= ""
-	for letter in inputstring:
-		for count in numberoftimes:
-			
-	return xtra
+    xtra= ""
+    for letter in inputstring:
+        for count in range(numberoftimes):
+            xtra= xtra + letter 
+    return xtra
+mux_letters("hi",3)
+print(mux_letters("hi",3))
+	
 
 
 
